5484151023c350dd2cf638d34034ed84ebbb2444
signor1 <emmanuelomemgboji@gmail.com>
2025-09-08 20:18:03 +0100

    feat: integration

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -3,17 +3,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <div className="max-w-7xl w-full mx-auto flex items-center justify-between h-16">
-      <p className="text-sm text-gray-500">
-        Learn how to build this on{" "}
-        <Link
-          href="https://learnweb3.io/courses/arbitrum-stylus-course"
-          target="_blank"
-          className="text-blue-500 hover:text-blue-600"
-        >
-          LearnWeb3 x Arbitrum Stylus
-        </Link>
-      </p>
+    <div className="max-w-7xl w-full mx-auto flex items-center justify-center h-16">
       <Link
         href={`https://sepolia.arbiscan.io/address/${CoinflipAddress}`}
         target="_blank"
